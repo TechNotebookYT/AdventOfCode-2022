6d0220176d8fb607f8465ffaae70904176e8f17b
Pranav Bala <technotebook@yahoo.com>
2022-12-13 17:28:20 -0600

    part to initial progress

---
+++ Day5/part2.py
@@ -0,0 +1,14 @@
+file = open('input.txt', 'r')
+lines = file.readlines()
+
+stack1 = ['S', 'M', 'R', 'N', 'W', 'J', 'V', 'T']
+stack2 = ['B', 'W', 'D', 'J', 'Q', 'P', 'C', 'V']
+stack3 = ['B', 'J', 'F', 'H', 'D', 'R', 'P']
+stack4 = ['F', 'R', 'P', 'B', 'M', 'N', 'D']
+stack5 = ['H', 'V', 'R', 'P', 'T', 'B']
+stack6 = ['C', 'B', 'P', 'T']
+stack7 = ['B', 'J', 'R', 'P', 'L']
+stack8 = ['N', 'C', 'S', 'L', 'T', 'Z', 'B', 'W']
+stack9 = ['L', 'S', 'G']
+
+
